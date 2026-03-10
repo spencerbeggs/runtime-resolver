4f32e4bb01a9696928aa2a07b99f0cc812f8540c
C. Spencer Beggs <spencer@savvyweb.systems>
2026-03-10 00:27:34 -0400

    chore: fix multi-target
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,10 +1,15 @@
 import { NodeLibraryBuilder } from "@savvy-web/rslib-builder";
 
 export default NodeLibraryBuilder.create({
-	transform({ pkg }) {
+	transform({ pkg, target }) {
+		if (target?.registry === "https://npm.pkg.github.com/") {
+			pkg.name = "@spencerbeggs/runtime-resolver";
+		}
 		delete pkg.devDependencies;
+		delete pkg.bundleDependencies;
 		delete pkg.scripts;
 		delete pkg.publishConfig;
+		delete pkg.devEngines;
 		delete pkg.devEngines;
 		return pkg;
 	},
