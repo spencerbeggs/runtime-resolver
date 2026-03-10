1aa5cd8d5f3048b643e08a56db17f943e556407e
C. Spencer Beggs <spencer@beggs.codes>
2026-03-09 23:01:09 -0400

    feat: replace GitHubTokenAuth with GitHubAutoAuth as default layer
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- src/layers/index.ts
+++ src/layers/index.ts
@@ -1,12 +1,12 @@
 import { Layer } from "effect";
 import { BunResolverLive } from "./BunResolverLive.js";
 import { DenoResolverLive } from "./DenoResolverLive.js";
+import { GitHubAutoAuth } from "./GitHubAutoAuth.js";
 import { GitHubClientLive } from "./GitHubClientLive.js";
-import { GitHubTokenAuth } from "./GitHubTokenAuth.js";
 import { NodeResolverLive } from "./NodeResolverLive.js";
 import { VersionCacheLive } from "./VersionCacheLive.js";
 
-export const GitHubLayer = GitHubClientLive.pipe(Layer.provide(GitHubTokenAuth));
+export const GitHubLayer = GitHubClientLive.pipe(Layer.provide(GitHubAutoAuth));
 export const SharedLayer = Layer.merge(GitHubLayer, VersionCacheLive);
 
 export const NodeLayer = NodeResolverLive.pipe(Layer.provide(SharedLayer));
